d2e9df8f77a2e9544745840021b2edb506349f38
juancarlos318504 <43257439+juancarlos318504@users.noreply.github.com>
2020-08-23 20:39:05 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,45 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360-ph",
+      "name": "360 ph",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.5548082023605794,
+        "pitch": 0.05440549016063656,
+        "fov": 1.2809123973965084
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.399622291149921,
+          "pitch": 0.11745938041770643,
+          "title": "Kaleido Lab",
+          "text": "Completamos y aumentamos tu mundo real, superponiendo contenido y capas de experiencias sensoriales a tus entornos."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
